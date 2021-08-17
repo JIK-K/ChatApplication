b66c84e3697eb2a4e13ab0c046b530761ef01d61
JIK-JIK <kddnswlr@naver.com>
2021-08-17 15:44:32 +0900

    [ChatApplication] - CreateFile

---
+++ ChatApplication/src/chatapplication/ChatApplication.java
@@ -0,0 +1,12 @@
+package chatapplication;
+
+/**
+ *
+ * @author JIK
+ */
+public class ChatApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
